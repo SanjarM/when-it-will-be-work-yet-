7501e5ceaf2f183f05986645b3ff32ca92ee70c7
SanjarM <https://github.com/SanjarM/when-it-will-be-work-yet-.git>
2017-08-10 03:06:46 +0600

    1 Signed-off-by: SanjarM <https://github.com/SanjarM/when-it-will-be-work-yet-.git>

---
+++ when-it-will-be-work-yet-/tablejs.js
@@ -0,0 +1,38 @@
+var mySearch=prompt("Write a name");
+
+
+
+var myName1='Jon';
+var mySurname1='Jones';
+
+var myName2='Daniel';
+var mySurname2='Cormier';
+
+var myName3='Volkan';
+var mySurname3='Oezdemir';
+
+var myName4='Alexander';
+var mySurname4='Gustafsson';
+
+var myName5='Glover';
+var mySurname5='Teixeira';
+
+var Result1="найдено";
+var Result2="не найдено"
+
+
+if (mySearch== myName1||mySearch==mySurname1||mySearch==myName1+mySurname1 ||mySearch== myName2||mySearch==mySurname2||myName3==mySearch||mySearch==mySurname3||mySearch==myName4||mySearch==mySurname4||mySearch==myName5||mySearch==mySurname5 )
+    console.log(Result1)
+else console.log (Result2)
+
+
+
+
+
+
+
+
+
+
+
+
